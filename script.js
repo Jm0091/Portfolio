d034a15814050017c935944cbf396c09d6f46d9d
Jems Chaudhary <70021478+Jm0091@users.noreply.github.com>
2024-05-30 11:06:20 -0400

    Version 0.1 - init commit with Project section under construction.

---
+++ script.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+    GridOfTheHeader();
+
+
+    $(window).resize(function () {
+        var windowHeight = $(window).height();
+        var windowWidth = $(window).width();
+        $('#container').css({
+            'width': windowWidth + 'px',
+            'height': windowHeight + 'px'
+        });
+
+        // Now update the lines:
+        GridOfTheHeader();
+    });
+
+
+    function GridOfTheHeader() {
+        var container = $('#lines');
+        var linesDiv = $('#lines');
+        var containerWidth = container.width();
+        var containerHeight = container.height();
+
+        //console.log("containerHeight: " + containerHeight);
+        //console.log("widthcontainerWidth: " + containerWidth);
+
+
+        var numHorizontalLines = Math.floor(containerHeight / 14);
+        var numVerticalLines = Math.floor(containerWidth / 14);
+
+        //console.log("numHorizontalLines: " + numHorizontalLines);
+        //console.log("numHorizontalLines: " + numVerticalLines);
+
+        for (var i = 1; i <= numHorizontalLines; i++) {
+            var line = $('<div class="line horizontal"></div>');
+            line.css('top', i * 14);
+            linesDiv.append(line);
+        }
+
+        for (var i = 1; i <= numVerticalLines; i++) {
+            var line = $('<div class="line vertical"></div>');
+            line.css('left', i * 14);
+            linesDiv.append(line);
+        }
+    }
+
+
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    //$('#container').hide();
+
+
+    var scrollPosition = 0;
+
+    // Save scroll position before opening the modal
+    $('#projectModal').on('show.bs.modal', function () {
+        scrollPosition = $(window).scrollTop();
+    });
+
+    // Restore scroll position after the modal is shown
+    $('#projectModal').on('shown.bs.modal', function () {
+        $(window).scrollTop(scrollPosition);
+    });
+
+    // Ensure scroll position is restored after modal is hidden
+    $('#projectModal').on('hidden.bs.modal', function () {
+        $(window).scrollTop(scrollPosition);
+    });
+    
+});
