c87f83f439e43cd6784ff1d62115737e87f90a02
Jems Chaudhary <70021478+Jm0091@users.noreply.github.com>
2024-10-20 13:55:53 -0400

    #2.8: Website tab icon added.

--- script.js
+++ script.js
@@ -1,72 +1,77 @@
 $(document).ready(function () {
-    GridOfTheHeader();
+  GridOfTheHeader();
 
-
-    $(window).resize(function () {
-        var windowHeight = $(window).height();
-        var windowWidth = $(window).width();
-        $('#container').css({
-            'width': windowWidth + 'px',
-            'height': windowHeight + 'px'
-        });
-
-        // Now update the lines:
-        GridOfTheHeader();
+  $(window).resize(function () {
+    var windowHeight = $(window).height();
+    var windowWidth = $(window).width();
+    $("#container").css({
+      width: windowWidth + "px",
+      height: windowHeight + "px",
     });
 
+    // Now update the lines:
+    GridOfTheHeader();
+  });
 
-    function GridOfTheHeader() {
-        var container = $('#lines');
-        var linesDiv = $('#lines');
-        var containerWidth = container.width();
-        var containerHeight = container.height();
+  function GridOfTheHeader() {
+    var container = $("#lines");
+    var linesDiv = $("#lines");
+    var containerWidth = container.width();
+    var containerHeight = container.height();
 
-        //console.log("containerHeight: " + containerHeight);
-        //console.log("widthcontainerWidth: " + containerWidth);
+    //console.log("containerHeight: " + containerHeight);
+    //console.log("widthcontainerWidth: " + containerWidth);
 
+    var numHorizontalLines = Math.floor(containerHeight / 14);
+    var numVerticalLines = Math.floor(containerWidth / 14);
 
-        var numHorizontalLines = Math.floor(containerHeight / 14);
-        var numVerticalLines = Math.floor(containerWidth / 14);
+    //console.log("numHorizontalLines: " + numHorizontalLines);
+    //console.log("numHorizontalLines: " + numVerticalLines);
 
-        //console.log("numHorizontalLines: " + numHorizontalLines);
-        //console.log("numHorizontalLines: " + numVerticalLines);
-
-        for (var i = 1; i <= numHorizontalLines; i++) {
-            var line = $('<div class="line horizontal"></div>');
-            line.css('top', i * 14);
-            linesDiv.append(line);
-        }
-
-        for (var i = 1; i <= numVerticalLines; i++) {
-            var line = $('<div class="line vertical"></div>');
-            line.css('left', i * 14);
-            linesDiv.append(line);
-        }
+    for (var i = 1; i <= numHorizontalLines; i++) {
+      var line = $('<div class="line horizontal"></div>');
+      line.css("top", i * 14);
+      linesDiv.append(line);
     }
 
+    for (var i = 1; i <= numVerticalLines; i++) {
+      var line = $('<div class="line vertical"></div>');
+      line.css("left", i * 14);
+      linesDiv.append(line);
+    }
+  }
 
-    $('.navbar-nav>li>a').on('click', function(){
-        $('.navbar-collapse').collapse('hide');
+  $(".navbar-nav>li>a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  //$('#container').hide();
+
+  var scrollPosition = 0;
+
+  // Save scroll position before opening the modal
+  $("#projectModal").on("show.bs.modal", function () {
+    scrollPosition = $(window).scrollTop();
+  });
+
+  // Restore scroll position after the modal is shown
+  $("#projectModal").on("shown.bs.modal", function () {
+    $(window).scrollTop(scrollPosition);
+  });
+
+  // Ensure scroll position is restored after modal is hidden
+  $("#projectModal").on("hidden.bs.modal", function () {
+    $(window).scrollTop(scrollPosition);
+  });
+
+  //NEW CODE ----
+  const hexagons = document.querySelectorAll(".hexagon");
+
+  hexagons.forEach((hex) => {
+    hex.addEventListener("click", (event) => {
+      // Future logic when a hexagon is clicked
+      console.log("Hexagon clicked:", event.target.alt);
+      // Transition to the next part of the UI
     });
-
-    //$('#container').hide();
-
-
-    var scrollPosition = 0;
-
-    // Save scroll position before opening the modal
-    $('#projectModal').on('show.bs.modal', function () {
-        scrollPosition = $(window).scrollTop();
-    });
-
-    // Restore scroll position after the modal is shown
-    $('#projectModal').on('shown.bs.modal', function () {
-        $(window).scrollTop(scrollPosition);
-    });
-
-    // Ensure scroll position is restored after modal is hidden
-    $('#projectModal').on('hidden.bs.modal', function () {
-        $(window).scrollTop(scrollPosition);
-    });
-    
+  });
 });
