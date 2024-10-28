70ae7612d7f6935956b583a5dc912afd337ea774
Jems Chaudhary <70021478+Jm0091@users.noreply.github.com>
2024-10-27 20:38:25 -0400

    #3.0: New Style, new code, new portfolio

--- script.js
+++ script.js
@@ -1,77 +1,29 @@
-$(document).ready(function () {
-  GridOfTheHeader();
-
-  $(window).resize(function () {
-    var windowHeight = $(window).height();
-    var windowWidth = $(window).width();
-    $("#container").css({
-      width: windowWidth + "px",
-      height: windowHeight + "px",
-    });
-
-    // Now update the lines:
-    GridOfTheHeader();
-  });
-
-  function GridOfTheHeader() {
-    var container = $("#lines");
-    var linesDiv = $("#lines");
-    var containerWidth = container.width();
-    var containerHeight = container.height();
-
-    //console.log("containerHeight: " + containerHeight);
-    //console.log("widthcontainerWidth: " + containerWidth);
-
-    var numHorizontalLines = Math.floor(containerHeight / 14);
-    var numVerticalLines = Math.floor(containerWidth / 14);
-
-    //console.log("numHorizontalLines: " + numHorizontalLines);
-    //console.log("numHorizontalLines: " + numVerticalLines);
-
-    for (var i = 1; i <= numHorizontalLines; i++) {
-      var line = $('<div class="line horizontal"></div>');
-      line.css("top", i * 14);
-      linesDiv.append(line);
-    }
-
-    for (var i = 1; i <= numVerticalLines; i++) {
-      var line = $('<div class="line vertical"></div>');
-      line.css("left", i * 14);
-      linesDiv.append(line);
-    }
-  }
-
-  $(".navbar-nav>li>a").on("click", function () {
-    $(".navbar-collapse").collapse("hide");
-  });
-
-  //$('#container').hide();
-
-  var scrollPosition = 0;
-
-  // Save scroll position before opening the modal
-  $("#projectModal").on("show.bs.modal", function () {
-    scrollPosition = $(window).scrollTop();
-  });
-
-  // Restore scroll position after the modal is shown
-  $("#projectModal").on("shown.bs.modal", function () {
-    $(window).scrollTop(scrollPosition);
-  });
-
-  // Ensure scroll position is restored after modal is hidden
-  $("#projectModal").on("hidden.bs.modal", function () {
-    $(window).scrollTop(scrollPosition);
-  });
-
-  //NEW CODE ----
-  const hexagons = document.querySelectorAll(".hexagon");
-
-  hexagons.forEach((hex) => {
-    hex.addEventListener("click", (event) => {
-      // Future logic when a hexagon is clicked
-      console.log("Hexagon clicked:", event.target.alt);
-      // Transition to the next part of the UI
-    });
-  });
-});
+!(function (n) {
+  n("html").removeClass("no-js"),
+    n("header a").click(function (e) {
+      if (!n(this).hasClass("no-scroll")) {
+        e.preventDefault();
+        var t = n(this).attr("href"),
+          i = n(t).offset().top;
+        n("html, body").animate(
+          { scrollTop: i + "px" },
+          Math.abs(window.pageYOffset - n(t).offset().top) / 4
+        ),
+          n("header").hasClass("active") &&
+            n("header, body").removeClass("active");
+      }
+    }),
+    n("#to-top").click(function () {
+      n("html, body").animate({ scrollTop: 0 }, 400);
+    }),
+    n("#lead-down").click(function () {
+      var e = n("#lead").next().offset().top;
+      n("html, body").animate({ scrollTop: e + "px" }, 400);
+    }),
+    n("#mobile-menu-open").click(function () {
+      n("header, body").addClass("active");
+    }),
+    n("#mobile-menu-close").click(function () {
+      n("header, body").removeClass("active");
+    })
+})(jQuery);
